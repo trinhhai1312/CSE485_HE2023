bd596ca225ea5960f1893ceaad71f06bb9d81738
Trịnh Ngọc Hải <86247472+Wolford02@users.noreply.github.com>
2023-08-04 17:36:40 +0700

    update

---
+++ XProject/app/config/config.php
@@ -0,0 +1,9 @@
+<?php
+define('APP_ROOT', dirname(__FILE__,3));
+define('APP_ROOT_HTML', dirname(__FILE__,3).'/public');
+
+define('DOMAIN','http://localhost:8080/CSE485_HE2023/XProject/');
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'xproject');
+define('DB_USER', 'root');
+define('DB_PASS', '');
